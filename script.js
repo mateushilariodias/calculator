f146881440bec6bd1c775ffcb1520863c98eeaee
mateushilariodias <mateusilariodias@gmail.com>
2024-03-14 14:29:28 -0300

    implements logic for the calculator to work and solves problems that prevented new operations from being carried out with the results of previous operations

--- script.js
+++ script.js
@@ -1,50 +1,90 @@
 const { createApp } = Vue;
 
 createApp({
-    data(){
-        return{
-            display:'0',
-            currentNumber:null,
-            previousNumber:null,
-            operator:null,
+    data() {
+        return {
+            display: '0',
+            currentNumber: null,
+            previousNumber: null,
+            operator: null,
         }
     },
-    methods:{
-        handleButton(button){
-            switch(button){
-                case "*":
-                case "-":
-                case "+":
+    methods: {
+        handleButton(button) {
+            switch (button) {
+                case '*':
+                case '-':
+                case '+':
                 case '/':
-                    this.handleOperator(button)
-                    break
-                case ".":
-                    this.handleDecimal()
-                    break
-                case "=":
-                    this.handleEqual()
-                    break
-                case "AC":
-                    this.handleClear()
-                    break
+                    this.handleOperator(button);
+                    break;
+                case '.':
+                    this.handleDecimal();
+                    break;
+                case '=':
+                    this.handleEquals();
+                    break;
+                case 'AC':
+                    this.handleClear();
+                    break;
                 default:
-                    this.handleNumber(button)
+                    this.handleNumber(button);
             }
         },
-        handleOperator(){
-
+        calculate() {
+            const numberOne = parseFloat(this.previousNumber);
+            const numberTwo = parseFloat(this.currentNumber);
+            let result;
+            switch (this.operator) {
+                case '+':
+                    result = numberOne + numberTwo;
+                    break;
+                case '-':
+                    result = numberOne - numberTwo;
+                    break;
+                case '*':
+                    result = numberOne * numberTwo;
+                    break;
+                case '/':
+                    result = numberOne / numberTwo;
+                    break;
+            }
+            this.display = result.toString();
         },
-        handleDecimal(){
-
+        handleOperator(op) {
+            if (this.currentNumber !== null && this.previousNumber !== null && this.operator !== null) {
+                this.calculate();
+            }
+            this.operator = op;
+            this.previousNumber = this.currentNumber;
+            this.currentNumber = null;
         },
-        handleEqual(){
-
+        handleDecimal() {
+            if (!this.display.includes('.')) {
+                this.display += '.';
+            }
         },
-        handleClear(){
-
+        handleEquals() {
+            if (this.currentNumber !== null && this.previousNumber !== null && this.operator !== null) {
+                this.calculate();
+                this.operator = null;
+                this.previousNumber = null;
+                this.currentNumber = parseFloat(this.display);
+            }
         },
-        handleNumber(){
-
-        }
+        handleClear() {
+            this.display = '0';
+            this.currentNumber = null;
+            this.previousNumber = null;
+            this.operator = null;
+        },
+        handleNumber(num) {
+            if (this.display === '0' || this.currentNumber === null) {
+                this.display = num;
+            } else {
+                this.display += num;
+            }
+            this.currentNumber = parseFloat(this.display);
+        },
     }
-}).mount("#app")
+}).mount("#app");
